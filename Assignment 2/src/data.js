7f7bfe5022eb35dd19e4f28078e688da06f9426e
Doğacan Serin <dogacanserin@gmail.com>
2022-10-08 01:22:00 +0300

    Shopping-Cart

---
+++ Assignment 2/src/data.js
@@ -0,0 +1,41 @@
+const data = {
+  products: [
+    {
+      id: "1",
+      name: "MacBook",
+      price: 1400,
+      image: "https://picsum.photos/id/180/2400/1600",
+    },
+    {
+      id: "2",
+      name: "Toy Car",
+      price: 150,
+      image: "https://picsum.photos/id/111/4400/2656",
+    },
+    {
+      id: "3",
+      name: "MacBook Pro",
+      price: 2000,
+      image: "https://picsum.photos/id/180/2400/1600",
+    },
+    {
+      id: "4",
+      name: "MacBook Air",
+      price: 1000,
+      image: "https://picsum.photos/id/119/3264/2176",
+    },
+    {
+      id: "5",
+      name: "Guitar",
+      price: 250,
+      image: "https://picsum.photos/id/145/4288/2848",
+    },
+    {
+      id: "6",
+      name: "Skateboard",
+      price: 150,
+      image: "https://picsum.photos/id/157/6211/4862",
+    },
+  ],
+};
+export default data;
